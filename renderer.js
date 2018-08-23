0d091e103290abde445abf9c4df97506346d0130
Théo Guerin <theo.guerin.pro@gmail.com>
2018-08-23 13:54:08 +0200

    amélioration de l'affichage

--- renderer.js
+++ renderer.js
@@ -1,6 +1,16 @@
 const {desktopCapturer} = require('electron')
 
+var anim;
 
+var canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var padding = 5;
+var WIDTH = canvas.width -padding ;
+var HEIGHT = canvas.height -padding;
+var barNumber = 27;
+var barWidth = (WIDTH / barNumber) -ecart;
+var ecart = 10;
 
 onchange = function(stream) {
     console.log("here",stream);
@@ -20,20 +30,14 @@ onchange = function(stream) {
     src.connect(analyser);
     //analyser.connect(context.destination);
 
-    var barNumber = 27;
     analyser.fftSize = 512;
 
     var bufferLength = analyser.frequencyBinCount;
-    console.log(bufferLength);
     var bufferByBar = Math.round(bufferLength/barNumber);
     var dataArray = new Uint8Array(bufferLength);
 
-    var WIDTH = canvas.width;
-    var HEIGHT = canvas.height;
 
-    var ecart = 2;
 
-    var barWidth = (WIDTH / barNumber) -ecart;
     var barHeight;
     var x = 0;
 
@@ -43,14 +47,14 @@ onchange = function(stream) {
     console.log("barWidth",barWidth);
 
     function renderFrame() {
-        requestAnimationFrame(renderFrame);
+        anim = requestAnimationFrame(renderFrame);
 
-        x = 0;
+        x = padding;
 
         analyser.getByteFrequencyData(dataArray);
 
         ctx.fillStyle = "#0f0";
-        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         for (var i = 0; i < barNumber; i++) {
             barHeight = 0
@@ -58,6 +62,8 @@ onchange = function(stream) {
                 barHeight += dataArray[i*bufferByBar+j];
             }
             barHeight = barHeight/bufferByBar;
+
+            barHeight = barHeight/700*HEIGHT;
 
             var r = barHeight + (25 * (i/bufferLength));
             var g = 250 * (i/bufferLength);
@@ -68,10 +74,11 @@ onchange = function(stream) {
 
             ctx.fillStyle = fillStyle;
             // ctx.fillStyle = "#F00";
-            ctx.fillRect(x, HEIGHT - 2*barHeight, barWidth, 2*barHeight);
+            ctx.fillRect(x, HEIGHT - 2*barHeight - barWidth/2, barWidth, 2*barHeight);
 
             ctx.beginPath();
-            ctx.arc(x+(barWidth/2), HEIGHT - 2*barHeight , barWidth/2, 0, 2 * Math.PI, false);
+            ctx.arc(x+(barWidth/2), HEIGHT - 2*barHeight -barWidth/2 , barWidth/2, 0, 2 * Math.PI, false);
+            ctx.arc(x+(barWidth/2), HEIGHT - barWidth/2 , barWidth/2, 0, 2 * Math.PI, false);
             ctx.fill();
             ctx.lineWidth = 0;
             ctx.strokeStyle = fillStyle;
@@ -81,21 +88,37 @@ onchange = function(stream) {
             x += barWidth + ecart;
         }
     }
-
+    console.log("render");
     renderFrame();
     // });
 };
 
-
-navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video : false
-})
-.then(onchange)
-.catch((e) => handleError(e))
-function handleError (e) {
-    console.log(e)
+function start(){
+    if(anim){
+        window.cancelAnimationFrame(anim);
+    }
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video : false
+    })
+    .then((stream)=>{ onchange(stream); })
+    .catch((e) => handleError(e))
+    function handleError (e) {
+        console.log(e)
+    }
 }
+
+
+window.onresize = function() {
+    console.log("here");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    WIDTH = canvas.width -padding ;
+    HEIGHT = canvas.height -padding;
+    barWidth = (WIDTH / barNumber) -ecart;
+};
+
+start();
 
 
 // This file is required by the index.html file and will
